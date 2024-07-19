42afcbc54f82011a22ebcb5967001f6e5efdf433
Antoine Massias <massias.antoine.pro@gmail.com>
2024-07-19 13:33:42 +0200

    Fixed (again) DEBUG envirenment variable.

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('0', default=0)))
+DEBUG = bool(int(os.environ.get('DEBUG', default=0)))
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', default='').split(' ')
 
