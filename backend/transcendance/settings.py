c3212deea37a5eb34e415e9ba25020155643ebf4
Antoine Massias <massias.antoine.pro@gmail.com>
2024-07-25 17:29:58 +0200

    Added: 	- Django now used environment variables. 	- `aalms.duckdns.org` is now a registered demain name.

--- backend/transcendance/settings.py
+++ backend/transcendance/settings.py
@@ -21,15 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e6(68lqdprfxx^5jfe2uvi!f__u-(=obbhp2l-f&%vn!ma3#9w"
+SECRET_KEY = os.environ.get('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', default='0')))
 
-ALLOWED_HOSTS = [
-    "aalms.duckdns.org"
-]
-
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', default='').split(' ')
 
 # Application definition
 
