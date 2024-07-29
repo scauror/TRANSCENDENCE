0d4f3a29214aa4e0134cccde259652eb574e27b8
scauror <simon.carpentier29@outlook.fr>
2024-07-29 19:20:23 +0200

    Footerbar beautified
    Changed:
    	- Logo justified at right of screen
    	- Title justified at left of screen

--- frontend/src/components/FooterBar.vue
+++ frontend/src/components/FooterBar.vue
@@ -1,7 +1,8 @@
 <template>
 	<nav class="footer">
+		<h1 class="title"> {{ $store.getters.getPageTitle }} </h1>
 		<router-link to="/">
-			<TranscendenceLogo class="minimized" />
+			<TranscendenceLogo />
 		</router-link>
 	</nav>
 </template>
@@ -11,13 +12,35 @@ import TranscendenceLogo from '@components/TranscendenceLogo.vue';
 </script>
 
 <style scoped>
-.minimized {
-	width: 5vh;
-	height: 5vh;
-}
 .footer {
 	display: flex;
 	align-items: center;
 	justify-content: center;
+}
+.title {
+	flex: 1;
+	color: var(--glow-color);
+	padding: 0.35em 1em;
+	border: 0.15em solid var(--glow-color);
+	border-radius: 0.45em;
+	background: none;
+	perspective: 2em;
+	text-align: begin;
+	letter-spacing: 0.6em;
+	font-family: "SpaceTron", sans-serif;
+	--size-factor: (0.00188323 * 70vw);
+	font-size: calc(12 * var(--size-factor));
+	font-weight: 900;
+	-webkit-box-shadow: inset 0px 0px 0.5em 0px var(--glow-color),
+		0px 0px 0.5em 0px var(--glow-color);
+	-moz-box-shadow: inset 0px 0px 0.5em 0px var(--glow-color),
+		0px 0px 0.5em 0px var(--glow-color);
+	box-shadow: inset 0px 0px 0.5em 0px var(--glow-color),
+		0px 0px 0.5em 0px var(--glow-color);
+	-webkit-text-shadow: 0 0 0.125em hsl(0 0% 100% / 0.3),
+		0 0 0.45em var(--glow-color);
+	-moz-text-shadow: 0 0 0.125em hsl(0 0% 100% / 0.3),
+		0 0 0.45em var(--glow-color);
+	text-shadow: 0 0 0.125em hsl(0 0% 100% / 0.3), 0 0 0.45em var(--glow-color);
 }
 </style>
