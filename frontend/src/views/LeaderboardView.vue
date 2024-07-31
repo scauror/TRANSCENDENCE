973db73f1b5f9572d244dd1281de01b9de10002e
scauror <simon.carpentier29@outlook.fr>
2024-07-31 15:26:29 +0200

    Leaderboard FrontEnd
    Added:
    	- Leaderboard View Style using a table

--- frontend/src/views/LeaderboardView.vue
+++ frontend/src/views/LeaderboardView.vue
@@ -5,14 +5,16 @@
 				<tr>
 					<th>Rank</th>
 					<th>Name</th>
-					<th>Score</th>
+					<th>Winrate</th>
+					<th>Rating</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr v-for="(player, index) in players" :key="player.id">
-					<td>{{ index + 1 }}</td>
+					<td>#{{ index + 1 }}</td>
 					<td>{{ player.name }}</td>
-					<td>{{ player.score }}</td>
+					<td>{{ player.winrate }}%</td>
+					<td>{{ player.score }} elo</td>
 				</tr>
 			</tbody>
 		</table>
@@ -21,58 +23,52 @@
 
 <script setup>
 const players = [
-	{ id: 1, name: 'Alice', score: 1500 },
-	{ id: 2, name: 'Bob', score: 1400 },
-	{ id: 3, name: 'Charlie', score: 1300 },
+	{ id: 1, name: 'Alice', winrate: 92, score: 1500 },
+	{ id: 2, name: 'Bob', winrate: 86, score: 1400 },
+	{ id: 3, name: 'Charlie', winrate: 75, score: 1300 },
 	// Exemple naze pour l'instant ici mais va falloir afficher l'entierete du tableau avec iterateur ou variable
 ];
 </script>
 
 <style scoped>
 .leaderboard {
-	max-width: 600px;
-	margin: auto;
+	margin: 1vh;
 	text-align: center;
-	font-family: "Raleway", sans-serif;
-	font-weight: bold;
-	color: hsl(0, 100%, 59%);
+	color: var(--glow-color);
+	font-family: "SpaceTron", sans-serif;
 	--size-factor: (0.00188323 * 70vw);
 	font-size: calc(12 * var(--size-factor));
 }
 
 table {
+	height: auto;
 	width: 100%;
-	border: 0.15em solid hsl(0, 100%, 59%);
-	border-collapse: separate;
-	border-spacing: 0;
-	margin-top: 50px;
-	background-color: black;
-	border-radius: 1em;
 	overflow: hidden;
-	color: hsl(0, 100%, 59%);
-	font-family: "Raleway", sans-serif;
-	text-transform: uppercase;
+	font-family: "SpaceTron", sans-serif;
 	-webkit-box-shadow: inset 0px 0px 0.5em 0px hsl(0, 100%, 59%),
 		0px 0px 0.5em 0px hsl(0, 100%, 59%);
 	-moz-box-shadow: inset 0px 0px 0.5em 0px hsl(0, 100%, 59%),
 		0px 0px 0.5em 0px hsl(0, 100%, 59%);
 	box-shadow: inset 0px 0px 0.5em 0px hsl(0, 100%, 59%),
 		0px 0px 0.5em 0px hsl(0, 100%, 59%);
-	animation: border-flicker 7s linear infinite;
 }
+
 
 th,
 td {
+	background-color: rgba(104, 0, 0, 0.274);
 	padding: 10px;
-	border: 0.15em solid hsl(0, 100%, 59%);
-	font-weight: bold;
-	font-size: 25px;
 	-webkit-box-shadow: inset 0px 0px 0.5em 0px hsl(0, 100%, 59%),
-		0px 0px 0.5em 0px hsl(0, 100%, 59%);
+	0px 0px 0.5em 0px hsl(0, 100%, 59%);
 	-moz-box-shadow: inset 0px 0px 0.5em 0px hsl(0, 100%, 59%),
-		0px 0px 0.5em 0px hsl(0, 100%, 59%);
+	0px 0px 0.5em 0px hsl(0, 100%, 59%);
 	box-shadow: inset 0px 0px 0.5em 0px hsl(0, 100%, 59%),
-		0px 0px 0.5em 0px hsl(0, 100%, 59%);
+	0px 0px 0.5em 0px hsl(0, 100%, 59%);
+}
+
+th {
+	font-weight: bolder;
+	font-size: larger;
 }
 
 tbody tr:nth-child(even) {
@@ -81,21 +77,5 @@ tbody tr:nth-child(even) {
 
 tbody tr:nth-child(odd) {
 	background-color: black;
-}
-
-table thead tr:first-child th:first-child {
-	border-top-left-radius: 0.5em;
-}
-
-table thead tr:first-child th:last-child {
-	border-top-right-radius: 0.5em;
-}
-
-table tbody tr:last-child td:first-child {
-	border-bottom-left-radius: 0.5em;
-}
-
-table tbody tr:last-child td:last-child {
-	border-bottom-right-radius: 0.5em;
 }
 </style>
