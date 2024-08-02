a6c9c3f7dfcc5792c4db8477ed86545c49549891
Antoine Massias <massias.antoine.pro@gmail.com>
2024-08-02 16:06:35 +0200

    Fixed indentation.
    Fix:
    	- Indentation was made using
    		spaces AND tabs...
    
    Note:
    	- Louis si tu passes par la, mange mes
    		pieds.

--- frontend/src/views/play/TournamentView.vue
+++ frontend/src/views/play/TournamentView.vue
@@ -1,21 +1,20 @@
 <template>
 	<div class="tournament">
-	  <h1>Tournament Bracket</h1>
-	  <Bracket />
+		<h1>Tournament Bracket</h1>
+		<Bracket />
 	</div>
-  </template>
-  
-  <script setup>
-  import Bracket from '@/components/TournamentBracket.vue';
-  </script>
-  
-  <style scoped>
-  .tournament {
+</template>
+
+<script setup>
+import Bracket from '@/components/TournamentBracket.vue';
+</script>
+
+<style scoped>
+.tournament {
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	color: var(--glow-color);
 	text-shadow: 0 0 0.125em hsl(0 0% 100% / 0.3), 0 0 0.45em var(--glow-color);
-  }
-  </style>
-  
+}
+</style>
